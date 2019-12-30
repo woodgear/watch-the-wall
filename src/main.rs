a250e0180ada1adfc3d81d29f2d380dc99b31494
wucong <1875486458@qq.com>
2019-12-30 22:35:01 +0800

    add ability to read from cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
 use std::time::{Duration,Instant};
 use reqwest;
 use chrono::prelude::*;
+#[macro_use]
+extern crate clap;
 
+#[derive(Clap,Debug)]
+#[clap(version = "0.1", author = "废物点心")]
+struct Opts {
+    #[clap(short = "u", long = "url", default_value = "http://www.google.com")]
+    url:String,
+    #[clap(short = "p", long = "proxy")]
+    proxy:Option<String>
+}
+
+trait TimeExt {
+    fn ms(&self)->Duration;
+    fn s(&self)->Duration;
+
+}
+
+impl TimeExt for u32 {
+    fn ms(&self)->Duration {
+        return Duration::from_millis(*self as u64)
+    }
+    fn s(&self)->Duration {
+        return Duration::from_secs(*self as u64)
+    }
+}
 
 fn main() {
+    let opts: Opts = Opts::parse();
+    println!("ops {:?}",opts);
     let mut t = term::stdout().unwrap();
+    let url = opts.url;
+    let proxy = opts.proxy;
     loop {
-        let (status,spend_time) = time(curl_google);
+        let (status,spend_time) = time(2.s(),||curl(&url, &proxy));
         if status.is_ok() {
             t.fg(term::color::GREEN).unwrap();
             writeln!(t, "now {:?} success spend_time {:?}",now_str(),spend_time).unwrap();
@@ -21,29 +50,30 @@ fn now_str()->String {
     Local::now().to_rfc2822()
 }
 
-fn time<F>(f:F)-> (Result<(),failure::Error>,Duration)
+fn time<F>(at_least:Duration,f:F)-> (Result<(),failure::Error>,Duration)
 where F:Fn()->Result<(),failure::Error> {
     let instant = Instant::now();
     let res = f();
-    return (res,instant.elapsed())
+    let spend = instant.elapsed();
+    if at_least>spend {
+        std::thread::sleep(at_least-spend);
+    }
+    return (res,spend)
 }
 
-fn curl_google()->Result<(),failure::Error> {
-    let res = reqwest::Client::builder()
-    .proxy(reqwest::Proxy::all("http://127.0.0.1:18080")?)
+fn curl(url:&str,proxy:&Option<String>)->Result<(),failure::Error> {
+    let  mut res = reqwest::Client::builder();
+    if let Some(proxy) = proxy {
+        res = res.proxy(reqwest::Proxy::all(proxy)?);
+    }
+    let res = res
     .timeout(Duration::from_secs(60))
     .build()?
-    .get("http://google.com")
+    .get(url)
     .send()?;
 
     if res.status().as_u16() !=200 {
         return Err(failure::format_err!("{:?}",res))
     }
     Ok(())
-}
-
-#[test]
-fn test_curl_google() {
-    let res = curl_google();
-    println!("res {:?}",res);
 }
